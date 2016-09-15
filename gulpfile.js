5bd77f8fe233e3572bbab36773389f2bef0d14e5
Javier Campón <javier_campon@jce.do>
2016-09-15 11:53:48 -0400

    modified:   bower.json new file:   dist/mdButtonBar.min.js new file:   dist/mdButtonBar.min.js.map modified:   gulpfile.js modified:   package.json

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,39 @@
 (function() {
 	'use strinct';
 
+	var moduleName = 'mdButtonBar';
 	var gulp      = require('gulp'),
 	merge         = require('merge-stream'),
 	templateCache = require('gulp-angular-templatecache'),
+	rename        = require('gulp-rename'),
 	concat        = require('gulp-concat'),
+	sourcemaps    = require('gulp-sourcemaps'),
+	uglify        = require('gulp-uglify'),
 	clean         = require('gulp-clean');
 
-	gulp.task('build', function() {
+	gulp.task('clean', function() {
+		return gulp.src('dist', { read: false }).pipe(clean());
+	});
+
+	gulp.task('build-templates', [ 'clean' ], function() {
 		return merge(
-			gulp.src('src/js/mdButtonBar.js'),
-			gulp.src('src/templates/**/*.html')
+			gulp.src('src/js/' + moduleName + '.js'),
+			gulp.src('src/templates/' + moduleName + '.html')
 				.pipe(templateCache('templatesCache.js', {
 					root  : '/templates/',
-					module: 'mdButtonBar'
+					module: moduleName
 				})))
-		.pipe(concat('mdButtonBar.js'))
+		.pipe(concat(moduleName + '.js'))
 		.pipe(gulp.dest('dist'));
+	});
+
+	gulp.task('build', [ 'build-templates' ], function() {
+		return gulp.src('dist/' + moduleName + '.js')
+			.pipe(sourcemaps.init())
+				.pipe(uglify({ preserveComments: 'license' }))
+				.pipe(rename({ extname: '.min.js' }))
+			.pipe(sourcemaps.write('.'))
+			.pipe(gulp.dest('dist'))
 	});
 
 })();

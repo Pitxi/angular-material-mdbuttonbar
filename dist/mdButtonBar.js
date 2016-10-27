158b055adf8a08a4713960477163bb05f0d90691
Javier Campón <javier_campon@jce.do>
2016-10-27 15:18:19 -0400

    Actualizada la distribución.

--- dist/mdButtonBar.js
+++ dist/mdButtonBar.js
@@ -30,14 +30,25 @@
 				if (!this.options.buttons) {
 					this.options.buttons = [];
 				}
+
+				for (var index = 0; index < this.options.buttons.length; ++index) {
+					if (this.options.buttons[index].enable === undefined) {
+						this.options.buttons[index].enable = true;
+					}
+				}
 			}
 
+			/**
+			 * Habilita o inhabilita un botón atendiendo a su propiedad @c enable
+			 * @param index {Number} El índice del botón a comprobar.
+			 * @returns {Boolean} @c true si el botón está habilitado o @c false en caso contrario.
+			 */
 			this.isButtonEnabled = function(index) {
-				console.log(index);
+				var enableProperty = this.options.buttons[index].enable;
 
-				return false;
+				return angular.isFunction(enableProperty) ? enableProperty() : !!enableProperty;
 			}
 		}
 })();
 
-angular.module('mdButtonBar').run(['$templateCache', function($templateCache) {$templateCache.put('/templates/mdButtonBar.html','\n<style scoped>\n\tmd-button-bar button md-icon[md-font-set] {\n\t\tfont-size: 24px;\n\t}\n\n\tmd-button-bar button[disabled="disabled"] md-icon[md-font-set] {\n\t\tcolor: rgba(0, 0, 0, 0.25) !important;\n\t}\n</style>\n\n<md-button\n\tng-class="button.class"\n\tng-repeat="button in $ctrl.options.buttons"\n\tng-style="button.style"\n\tng-disabled="$ctrl.isButtonEnabled($index)"\n\tng-attr-aria-label="{{ button.label }}"\n\tng-attr-ng-roles="{{ button.roles }}"\n\tng-attr-ng-roles-comparer="{{ button.rolesComparer }}"\n\tng-click="$ctrl.onButtonClick({ button: button })">\n\t<md-tooltip\n\t\tng-if="button.tooltip"\n\t\tng-class="button.tooltip.class"\n\t\tng-attr-md-direction="{{ button.tooltip.direction }}"\n\t\tng-attr-md-delay="button.tooltip.delay || 0"\n\t\tng-style="button.tooltip.style">\n\t\t{{ button.tooltip.text || button.tooltip }}\n\t</md-tooltip>\n\t<md-icon\n\t\tng-if="button.icon && !button.icon.svgIcon"\n\t\tng-class="button.icon.class"\n\t\tng-attr-md-font-set="{{ button.icon.fontSet }}"\n\t\tng-attr-md-font-icon="{{ button.icon.fontIcon }}"\n\t\tng-attr-md-svg-src="{{ button.icon.svgSrc }}"\n\t\tng-style="button.icon.style">\n\t</md-icon>\n\t<md-icon\n\t\tng-if="button.icon && button.icon.svgIcon"\n\t\tng-class="button.icon.class"\n\t\tng-attr-md-svg-icon="{{ button.icon.svgIcon }}"\n\t\tng-style="button.icon.style">\n\t</md-icon>\n\t<span ng-if="button.text">{{ button.text }}</span>\n</md-button>');}]);
+angular.module('mdButtonBar').run(['$templateCache', function($templateCache) {$templateCache.put('/templates/mdButtonBar.html','\n<style scoped>\n\tmd-button-bar button md-icon[md-font-set] {\n\t\tfont-size: 24px;\n\t}\n\n\tmd-button-bar button[disabled="disabled"] md-icon[md-font-set] {\n\t\tcolor: rgba(0, 0, 0, 0.25) !important;\n\t}\n</style>\n\n<md-button\n\tng-class="button.class"\n\tng-repeat="button in $ctrl.options.buttons"\n\tng-style="button.style"\n\tng-disabled="!$ctrl.isButtonEnabled($index)"\n\tng-attr-aria-label="{{ button.label }}"\n\tng-attr-ng-roles="{{ button.roles }}"\n\tng-attr-ng-roles-comparer="{{ button.rolesComparer }}"\n\tng-click="$ctrl.onButtonClick({ button: button })">\n\t<md-tooltip\n\t\tng-if="button.tooltip"\n\t\tng-class="button.tooltip.class"\n\t\tng-attr-md-direction="{{ button.tooltip.direction }}"\n\t\tng-attr-md-delay="button.tooltip.delay || 0"\n\t\tng-style="button.tooltip.style">\n\t\t{{ button.tooltip.text || button.tooltip }}\n\t</md-tooltip>\n\t<md-icon\n\t\tng-if="button.icon && !button.icon.svgIcon"\n\t\tng-class="button.icon.class"\n\t\tng-attr-md-font-set="{{ button.icon.fontSet }}"\n\t\tng-attr-md-font-icon="{{ button.icon.fontIcon }}"\n\t\tng-attr-md-svg-src="{{ button.icon.svgSrc }}"\n\t\tng-style="button.icon.style">\n\t</md-icon>\n\t<md-icon\n\t\tng-if="button.icon && button.icon.svgIcon"\n\t\tng-class="button.icon.class"\n\t\tng-attr-md-svg-icon="{{ button.icon.svgIcon }}"\n\t\tng-style="button.icon.style">\n\t</md-icon>\n\t<span ng-if="button.text">{{ button.text }}</span>\n</md-button>');}]);
